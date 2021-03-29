9f3c5f9845a06e4b6605559ad3183ab173b6d1f5
지운 김 <kkimj@hanyang.ac.kr>
2021-03-30 01:09:34 +0900

    update main.py for args handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import argparse
 from smartwheelchair import SmartWheelChair
 
-if __name__ == '__main__':
+
+def main(args):
     print('SmartWheelChair now running...')
-    SmartWheelChair().Run()
+    SmartWheelChair().Run(is_multithreading = args.thread)
+    # print(args)
+    # print(args.thread, args.bluetooth, args.joystick, args.testmode)
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--joystick', '-joy', help='Control by joystick', action="store_true")
+    parser.add_argument('--bluetooth', '-bt', help='Use Bluetooth Protocol for Controller', action="store_true")
+    parser.add_argument('--testmode', '-test', help='Test mode', action="store_true")
+    parser.add_argument('--thread', '-thread', help='Use Multithreading for input handling', action="store_true")
+    
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = get_arguments()
+    main(args)
+
