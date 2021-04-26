abc736f2273aae3854d20a9fef0c1635692b4a4b
= <kkimj@hanyang.ac.kr>
2021-04-26 17:59:52 +0900

    update threading_test.py

--- test/threading_test.py
+++ test/threading_test.py
@@ -1,6 +1,7 @@
 import threading
 import curses
 import time
+import random
 
 command = None
 command_lock = threading._allocate_lock()
@@ -9,24 +10,120 @@ def input_handler():
     print('input_handler')
     # do not wait for input when calling getch
     global command, command_lock
+    tmp_command = None
+
+    stdscr = curses.initscr()
+    stdscr.nodelay(1)
+
     while True:
-        c = input().strip()
+        c = stdscr.getch()
+        if c == -1:
+            continue
+        c = chr(c)
+
+        if c == 'w':
+            tmp_command = 'forward'
+        elif c == 'a':
+            tmp_command = 'turn_left'
+        elif c == 's':
+            tmp_command = 'stop'
+        elif c == 'd':
+            tmp_command = 'turn_right'
+        elif c == 'x':
+            tmp_command = 'backward'
+        elif c == 'q':
+            tmp_command = 'quit'
+        else:
+            continue
+
         with command_lock:
-            command = c
+            command = tmp_command
+            time.sleep(1)
+        stdscr.refresh()
+        # stdscr.clear()
+        stdscr.move(0, 0)
+        
+        if command == 'quit':
+            curses.endwin()
+            break
+        
         # time.sleep(1)
 
 def motor_handler():
     global command, command_lock
+    prev_command = None
     while True:
         while not command:
             time.sleep(0.01)
-        with command_lock:
-            print(command, end="motor_handler\n")
-            command = None
+
+        if prev_command == command:
+            continue
+
+        # with command_lock:
+        #   pass
+        print(command, end="motor_handler\n")
+        
+        prev_command = command
+        
+        if command == 'quit':
+                break
+            # command = None
         # time.sleep(1)
 
-def obstacle_hanler():
-    pass
+def obstacle_handler():
+    global command, command_lock
+    time_offset = 0.85
+    tmp_command = None
+    while True:
+        direction = random.choice(['front', 'right', 'left', 'near'])
+        obstacle_status = {
+            'front' : False,
+            'left' : False,
+            'right' : False,
+            'near' : False,
+        }
+        if True:
+            # print('obstacle_status', obstacle_status)
+            pass
+
+        if obstacle_status['front']:
+            # break
+            if obstacle_status['left'] and obstacle_status['right']:
+                tmp_command = 'backward'
+
+            elif obstacle_status['left']:
+                tmp_command = 'turn_right'
+
+            
+            elif obstacle_status['right']:
+                tmp_command = 'turn_left'
+
+            else:
+                tmp_command = 'turn_left'
+
+        elif obstacle_status['left']:
+            tmp_command = 'turn_right'
+
+            
+        elif obstacle_status['right']:
+            tmp_command = 'turn_left'
+
+
+        elif obstacle_status['near']:
+            tmp_command = 'forward'
+
+        # Safe from obstacle
+        else:
+            tmp_command = 'forward'
+
+        with command_lock:
+            command = tmp_command
+            tmp_command = None
+        time.sleep(1)
+        if command == 'quit':
+                break
+        
+    
 
 if __name__ == '__main__':
     # 데몬 쓰레드
@@ -38,9 +135,15 @@ if __name__ == '__main__':
     motor_thread.daemon = True
     motor_thread.start()
     
+    obstacle_thread = threading.Thread(target=obstacle_handler, args=())
+    obstacle_thread.daemon = True
+    obstacle_thread.start()
+
+    # global command
     while True:
         try:
-            pass
+            if command == 'quit':
+                break
         except KeyboardInterrupt:
             print("### End ###")
             break
